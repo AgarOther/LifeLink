7ebe5792e28035b4e2ce59b72e446965a36c4408
Agar <46058590+AgarOther@users.noreply.github.com>
2025-03-30 03:06:48 +0200

    Ajout de commentaires

--- java/me/eleo/lifelink/Events.java
+++ java/me/eleo/lifelink/Events.java
@@ -22,12 +22,14 @@ public class Events implements Listener {
 
         // Change la barre de vie liée selon les dégâts reçus
         if (e.getEntityType() == EntityType.PLAYER) {
+            // On récupère les dégâts avec la formule: (barre de vie liée - dégâts subis)
             double damage = LifeLink.sharedLife - e.getFinalDamage();
             // Si les dégâts sont inférieurs à 0, on les mets à 0 pour ne pas aller en négatif, et on met à jour la barre de vie liée
             LifeLink.sharedLife = damage > 0 ? damage : 0;
             // On envoie le son de prise de dégâts à chaque joueur
             for (Player p : Bukkit.getOnlinePlayers())
             {
+                // Ne pas jouer le son si le joueur actuellement dans la boucle est le joueur ayant déclenché l'événement (pour ne pas qu'il l'entende 2 fois)
                 if (e.getEntity() == p)
                     continue ;
                 p.playSound(p, Sound.ENTITY_PLAYER_HURT, SoundCategory.PLAYERS, 1.0F, 0.85F + (float) (Math.random() * 0.3));
